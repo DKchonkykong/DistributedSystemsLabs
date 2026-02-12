6e149d5989d92e66d669720f7dc33a401d49c475
AdrianV2022 <146434555+DKchonkykong@users.noreply.github.com>
2026-02-12 10:18:07 +0000

    did the first part sort of works yay

--- Lab3-ThreadsCode/WPFApplicationForumLAB3/WPFApplicationForumLAB3/MainWindow.xaml.cs
+++ Lab3-ThreadsCode/WPFApplicationForumLAB3/WPFApplicationForumLAB3/MainWindow.xaml.cs
@@ -16,9 +16,39 @@ namespace WPFApplicationForumLAB3
     /// </summary>
     public partial class MainWindow : Window
     {
+        public List<int> primeNumbers;
+        public TextBox outputTextBox;
         public MainWindow()
         {
             InitializeComponent();
+            primeNumbers = new List<int>();
+        }
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            FindPrimeNumbers(20000);
+            outputTextBox.Text = primeNumbers[9999].ToString();
+        }
+        private void FindPrimeNumbers(int numberOfPrimesToFind)
+        {
+            int primeCount = 0; int currentPossiblePrime = 1;
+            while (primeCount < numberOfPrimesToFind)
+            {
+                currentPossiblePrime++; int possibleFactor = 2; bool isPrime = true;
+                while ((possibleFactor <= currentPossiblePrime / 2) && (isPrime == true))
+                {
+                    int possibleFactor2 = currentPossiblePrime / possibleFactor;
+                    if (currentPossiblePrime == possibleFactor2 * possibleFactor)
+                    {
+                        isPrime = false;
+                    }
+                    possibleFactor++;
+                }
+                if (isPrime)
+                {
+                    primeCount++;
+                    primeNumbers.Add(currentPossiblePrime);
+                }
+            }
         }
     }
 }
