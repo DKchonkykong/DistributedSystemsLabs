1062d5dc1d9cbb58d3a19a93e6cbabb3fdba5a70
AdrianV2022 <146434555+DKchonkykong@users.noreply.github.com>
2026-02-26 14:33:05 +0000

    done the first task I think need to do the rest now at least it is working since I had to config the startup so two projects run at once

--- Lab4-AsyncCode/Async/EntryPoint.cs
+++ Lab4-AsyncCode/Async/EntryPoint.cs
@@ -1,8 +1,28 @@
 ﻿using Client;
 using System;
 using System.Threading.Tasks;
+using ClientBehaviours = Client.ClientBehaviours;
 
 // Add code here to send requests
+
+Console.WriteLine("Enter endpoint (e.g. /ping), or blank to exit:");
+
+while (true)
+{
+    Console.Write("Endpoint: ");
+    string? endpoint = Console.ReadLine();
+
+    if (string.IsNullOrWhiteSpace(endpoint))
+    {
+        break;
+    }
+
+    Console.Write("Message: ");
+    string? message = Console.ReadLine() ?? string.Empty;
+
+    // This will connect to 127.0.0.1:5000 and send the request using your existing serializer
+    ClientBehaviours.SendRequest(message, endpoint);
+}
 
 Console.WriteLine("Execution finished");
 Console.ReadLine();
